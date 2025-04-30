cae300e9562a00300379f40d50cf1d92d839572a
yausername2 <yausername@tuta.io>
2025-04-30 11:52:29 +1000

    Revised help messages

--- msgbox.ps1
+++ msgbox.ps1
@@ -6,18 +6,18 @@ This script is designed to run in CrowdStrike RTR (Real Time Response) or anothe
 The script uses the `msg.exe` utility, which is available in the Professional and Enterprise editions of Windows, to display a hardcoded message box. 
 It repeatedly executes the command 50 times to ensure the message is displayed prominently on the target system.
 
-# NOTES
-- `msg.exe` is not available in the Home edition of Windows, so this script will not work on systems running that edition.
-- This script is intended for use in incident response scenarios where immediate user notification is required.
-- The message is hardcoded and should be modified to suit the specific incident or alert being communicated.
-
-# USE CASE
+### USE CASE #
 Run the script in an RTR session to notify users of a security incident, such as containing a compromised endpoint in the network and alerting the user to contact the security team.
 The hardcoded message will inform the user that their system has been contained in the network due to a detected security threat:
 
  [ SECURITY ALERT ] Your system has been contained in the network due to a detected security threat. Contact the Security team immediately at: security@company.com
 
 The message will be displayed 50 times to ensure visibility.
+
+.NOTES
+- `msg.exe` is not available in the Home edition of Windows, so this script will not work on systems running that edition.
+- This script is intended for use in incident response scenarios where immediate user notification is required.
+- The message is hardcoded and should be modified to suit the specific incident or alert being communicated.
 
 .LINK
 https://github.com/yausername2/CrowdStrike-RTR-Scripts/blob/main/msgbox.ps1
